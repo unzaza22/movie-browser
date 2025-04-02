be1480e62946fdcf70cb4361a13b85fde932ac42
unzaza22 <suwunorder@gmail.com>
2025-04-02 16:31:56 +0700

    Section 5.22 New Hosting
    Edit app.routes.server.ts give 'movie-details/:imdID' with SSR mode.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'movie-details/:imdID',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
